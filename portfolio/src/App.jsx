2f9977cf04225576840b9aa8bb6116bc3d1f51ec
andy <eaadenutsi@st.ug.edu.gh>
2024-09-25 01:58:33 +0000

    progress added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Project from './components/project'
 import About from './components/about';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <About />
       <section id='projects'>
         <h1>Projects</h1>
---
+++ portfolio/src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+//creating the navbar component
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="#home">MyPortfolio</a>
+      </div>
+      <ul className="navbar-links">
+        <li><a href="#about">About Me</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
